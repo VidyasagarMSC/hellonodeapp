412779d53f7ab9082e48633f226b839f5b390a27
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2019-09-13 17:53:29 +0530

    Updated the app

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-const express = require('express')
-
-const app = express()
-
-// First route
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello world' })
-})
-
-app.listen('3000')
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080, 'APP_PRIVATE_IP_ADDRESS');
+console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
