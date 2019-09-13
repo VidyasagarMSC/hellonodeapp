4e79ef67cbc3467d547d25e72dab5ef9cf02f640
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2019-09-13 17:43:25 +0530

    Updated the app

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('Hello World!\n');
-});
+const app = express()
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+// First route
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello world' })
+})
+
+app.listen('3000')
